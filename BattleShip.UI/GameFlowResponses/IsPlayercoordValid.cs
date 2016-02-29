255c717202ce1099435e6651ead92847bac5fdcb
Rocco Riccardi <Rocco.Riccardi@amtrustgroup.com>
2016-02-29 15:32:56 -0500

    fixed bug in coordinate check

--- BattleShip.UI/GameFlowResponses/IsPlayercoordValid.cs
+++ BattleShip.UI/GameFlowResponses/IsPlayercoordValid.cs
@@ -8,35 +8,37 @@ namespace BattleShip.UI.GameFlowResponses
 {
     public class IsPlayercoordValid
     {
-        private char[] verifyAgainstChar = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
-        //private int[] verifyAgainstInt = {1, 2, 3, 4, 5, 6, 7, 8, 9}; Using Int32.TryParse instead.
+        private char[] verifyAgainstChar = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 
         public bool IsItGood(string a)
         {
-            if (a.Length == 0 || a.Length < 2 || a.Length > 3) //If length isn't reasonable, toss out immediately.
+            if (a.Length == 0 || a.Length < 2 || a.Length > 3)
             {
                 return false;
             }
-            else
+
+            int check = 0;
+            for (int i = 0; i < verifyAgainstChar.Length - 1; i++)
             {
-                int check = 0;
-                int good = 0;
-                for (int i = 0; i < verifyAgainstChar.Length -1; i++)
+                int good;
+                if (a[0] == verifyAgainstChar[i]) //Ensure first char is letter a-j
                 {
-                    if (a[0] == verifyAgainstChar[i]) //Ensure first char is letter a-j
-                    {
-                        check+=1;
-                    }
-                    if (Int32.TryParse(a.Substring(1,1), out good)) 
-                    {  
-                        check+= 1; //Ensure 2nd string element is an int. out good is irrelevent.
-                    }
-                    if (check == 2) //Two checks means we're good.
-                    {
-                        return true;
-                    }
+                    check++;
+                }
+                if (int.TryParse(a.Substring(1, 1), out good))
+                {
+                    check++; //Ensure 2nd string element is an int. out good is irrelevent.
+                }
+                if (int.TryParse(a.Substring(1, 2), out good) && good == 10)
+                {
+                    check++;
+                }
+                if (check == 2) //Two checks means we're good.
+                {
+                    return true;
                 }
             }
+
             return false;
         }
     }
